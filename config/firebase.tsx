05c5354210d722eef6c7dc7da77d05cced825a3b
edwarddikgale <edward.dikgale@gmail.com>
2021-07-18 14:43:15 +0200

    Update firebase.tsx

--- config/firebase.tsx
+++ config/firebase.tsx
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCcaH_CML3NO_oeuaTj1bWEqsqBzH-RC-o",
-    authDomain: "restock-home.firebaseapp.com",
-    databaseURL: "https://restock-home.firebaseio.com",
-    projectId: "restock-home",
-    storageBucket: "restock-home.appspot.com",
-    messagingSenderId: "1052301657490",
-    appId: "1:1052301657490:web:806ccec1c11c8356892d4b",
-    measurementId: "G-2GJJLRQTN3"
+    apiKey: "xxx",
+    authDomain: "xxx",
+    databaseURL: "xxx",
+    projectId: "xxx",
+    storageBucket: "xxx",
+    messagingSenderId: "xxx",
+    appId: "xxx",
+    measurementId: "xxx"
   };
 
   export default firebaseConfig;
